a95c128abdb77c17deebdc9676dc22eede23c7df
arnoldshek <ashek@hawaii.edu>
2022-03-15 18:46:14 -1000

    added traversal command

--- src/main/java/frc/robot/commands/ClimbToTraversal.java
+++ src/main/java/frc/robot/commands/ClimbToTraversal.java
@@ -6,21 +6,31 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Climber;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class ClimbToTraversal extends SequentialCommandGroup {
-  Climber s_climber;
-  Joystick joystick;
   /** Creates a new ClimbToTraversal. */
-  public ClimbToTraversal(Climber climber, Joystick joystick) {
+  public ClimbToTraversal(Climber s_climber, Joystick joystick) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new MidLift(s_climber),
-      
+      new WaitUntilCommand(s_climber::getMidClawEngaged),
+      new MidGrab(s_climber),
+      new WaitUntilCommand(s_climber::getJoystickButton),
+      new MidLockWinch(s_climber),
+      new WaitUntilCommand(s_climber::getJoystickButton),
+      new MidPull(s_climber),
+      new WaitUntilCommand(s_climber::getJoystickButton),
+      new TraverseLift(s_climber),
+      new WaitUntilCommand(s_climber::getTraverseClawEngaged),
+      new TraverseGrab(s_climber),
+      new WaitUntilCommand(s_climber::getJoystickButton),
+      new ReleaseMidClaw(s_climber)
     );
   }
 }
